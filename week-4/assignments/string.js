e9204640c421d5920c5ca1d4f48edae6bc963225
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-08-25 13:38:39 +0200

    Add week 4 string assignments solutions

---
+++ week-4/assignments/string.js
@@ -0,0 +1,18 @@
+// Assignment 1
+let userName = 'Elzero';
+console.log(userName[0].toLowerCase()); // e
+console.log(userName.charAt(0).toLowerCase()); // e
+console.log(userName.slice(0, 1).toLowerCase()); // e
+console.log(userName.substring(0, 1).toLowerCase()); // e
+console.log(userName.split('')[0].toLowerCase()); // e
+console.log(userName.substr(0, 1).toLowerCase().repeat(3)); // eee
+
+// Assignment 2
+let word = 'Elzero';
+let letterZ = 'z';
+let letterE = 'e';
+let letterO = 'O';
+
+console.log(word.includes(letterZ)); // True
+console.log(word.startsWith(letterE.toUpperCase())); // True
+console.log(word.endsWith(letterO.toLowerCase())); // True
