2ba3be19dde4f52467f198e0d8a26fd40772749f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2022-08-28 16:15:34 +0200

    Change to subtraction operator

--- week-6/assignments/40-47.js
+++ week-6/assignments/40-47.js
@@ -3,7 +3,7 @@ let myFriends = ['Ahmed', 'Elham', 'Osama', 'Gamal'];
 let num = 3;
 
 // Method 1
-console.log(myFriends.slice(num % num, num)); // ["Ahmed", "Elham", "Osama"];
+console.log(myFriends.slice(num - num, num)); // ["Ahmed", "Elham", "Osama"];
 
 // Method 2
 myFriends.pop(); // myFriends.length = 3
