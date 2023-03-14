52f0b67a5e92f9891184d3764d307fa5837e230b
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-14 20:06:13 +0200

    feat: add week 17 assignment 2 solution

--- week-17/assignments/123-133.js
+++ week-17/assignments/123-133.js
@@ -3,3 +3,12 @@ let setOfNumbers = new Set().add(10);
 setOfNumbers.add(20).add(setOfNumbers.size);
 console.log(setOfNumbers);
 console.log([...setOfNumbers][2]);
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 2
+let myFriends = ['Osama', 'Ahmed', 'Sayed', 'Sayed', 'Mahmoud', 'Osama'];
+
+// Needed Output
+console.log([...new Set(myFriends)].sort());
+// (4) ['Ahmed', 'Mahmoud', 'Osama', 'Sayed']
