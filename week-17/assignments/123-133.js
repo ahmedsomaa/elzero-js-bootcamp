c6af2d722fc3ef45de09236919b7d5ae1cb3ff7f
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-14 20:08:39 +0200

    feat: add week 17 assignment 3 solution

--- week-17/assignments/123-133.js
+++ week-17/assignments/123-133.js
@@ -12,3 +12,21 @@ let myFriends = ['Osama', 'Ahmed', 'Sayed', 'Sayed', 'Mahmoud', 'Osama'];
 // Needed Output
 console.log([...new Set(myFriends)].sort());
 // (4) ['Ahmed', 'Mahmoud', 'Osama', 'Sayed']
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 3
+let myInfo = {
+  username: 'Osama',
+  role: 'Admin',
+  country: 'Egypt'
+};
+
+let map = new Map(Object.entries(myInfo));
+console.log(map);
+console.log(map.size);
+console.log(map.has('role'));
+// Needed Output
+//   Map(3) {'username' => 'Osama', 'role' => 'Admin', 'country' => 'Egypt'}
+//   3
+//   true
