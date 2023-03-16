3779e340dbd7b66837d42cd7b16f061c821d9b18
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-16 14:14:55 +0200

    feat: add week 18 assignment 2 solution

--- week-18/assignments/134-146.js
+++ week-18/assignments/134-146.js
@@ -3,3 +3,13 @@ let ip = '2001:db8:3333:4444:5555:6666:7777:8888';
 let ipRe = /\d{4}:\w+\d:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}:\d{4}/gi;
 
 console.log(ipRe.test(ip)); // true
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 2
+let specialNames = 'Os10O OsO Os100O Osa100O Os1000 Os100m';
+let specialNamesRe = /Os\d*O/gi;
+console.log(specialNames.match(specialNamesRe));
+
+// Output
+// ['Os10O', 'OsO', 'Os100O']
