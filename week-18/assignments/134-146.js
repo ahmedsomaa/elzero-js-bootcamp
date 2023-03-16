4d683596abd6cc731d0dbe63ca548d50b3e44162
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-16 14:44:55 +0200

    feat: add week 18 assignment 4 solution

--- week-18/assignments/134-146.js
+++ week-18/assignments/134-146.js
@@ -21,3 +21,23 @@ let phone = '+(995)-123 (4567)';
 let phoneRe = /\+\(\d{3}\)-\d{3}\s\(\d{4}\)/gi;
 
 console.log(phoneRe.test(phone)); // true
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 4
+let re = /https?:\/\/(?:[-\w]+\.)?([-\w]+)\.\w+(?:\.\w+)?\/?.*/i;
+// http
+// s -> zero or one
+// ://
+// (?:[-\w]+\.)? -> one of eithe (wordcharacter or dash) one or more times followed by a dot for example: www.
+// ([-\w]+) -> one of (wordcharacter or -) one or more times
+//  \. -> dot
+// \w+ -> wordcharcater one or more times
+// (?:\.\w+)? -> fot followed by a wordcharacter one or more times
+// /
+// ?.* -> any character except new line 0 or more times
+// i -> ignore case
+// use tools like this to visualize a regex pattern:
+// https://jex.im/regulex/#!flags=&re=%5E(a%7Cb)*%3F%24
+// https://regexper.com/
+// https://regexr.com/
