9b19730b2179943347afa1f3dba4cfe4c79f4fef
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-18 00:26:13 +0200

    feat: add week 19 assignment 5 solution

--- week-19/assignments/147-158.js
+++ week-19/assignments/147-158.js
@@ -68,3 +68,42 @@ String.prototype.addLove = function () {
 // Do Not Edit Below
 let myStr = 'Elzero';
 console.log(myStr.addLove()); // I Love Elzero Web School
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 5
+const myObj = {
+  username: 'Elzero',
+  id: 100,
+  score: 1000,
+  country: 'Egypt'
+};
+
+// Write Your Code Here
+Object.defineProperties(myObj, {
+  score: {
+    writable: false
+  },
+  id: {
+    enumerable: false
+  },
+  country: {
+    writable: false,
+    enumerable: false,
+    configurable: false
+  }
+});
+
+myObj.score = 500;
+
+for (let prop in myObj) {
+  console.log(`${prop} => ${myObj[prop]}`);
+}
+
+console.log(myObj);
+
+// Needed Output
+
+//   "username => Elzero"
+//   "score => 1000"
+//   {username: 'Elzero', score: 1000, id: 100}
