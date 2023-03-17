2ea104048c9e7beef71dcef3ebec3fec58d1fc5b
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-17 23:52:51 +0200

    feat: add week 19 assignment 2 solution

--- week-19/assignments/147-158.js
+++ week-19/assignments/147-158.js
@@ -21,3 +21,39 @@ const car3 = new Car('Mercedes', '2023', 500000);
 
 console.log(`Car One Name Is ${car1.n} And Model Is ${car1.m} And Price Is ${car1.p}`);
 console.log(car1.run());
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 2
+class Phone {
+  constructor(name, serial, price) {
+    this.name = name;
+    this.serial = serial;
+    this.price = price;
+  }
+}
+
+class Tablet extends Phone {
+  constructor(name, serial, price, size = 'Unknown') {
+    super(name, serial, price);
+    this.size = size;
+  }
+
+  fullDetails() {
+    return `${this.name} Serial is ${this.serial} And Size Is ${this.size}`;
+  }
+}
+
+let TabletOne = new Tablet('iPad', 100200300, 1500, 12.9);
+let TabletTwo = new Tablet('Nokia', 350450650, 800, 10.5);
+let TabletThree = new Tablet('LG', 250450650, 650);
+
+
+console.log(`${TabletOne.fullDetails()}`);
+// iPad Serial is 100200300 And Size Is 12.9
+
+console.log(`${TabletTwo.fullDetails()}`);
+// Nokia Serial is 350450650 And Size Is 10.5
+
+console.log(`${TabletThree.fullDetails()}`);
+// LG Serial is 250450650 And Size Is Unknown
