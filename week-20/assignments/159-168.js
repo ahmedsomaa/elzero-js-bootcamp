52c1d262d816eb3bfe51f7f96f8319bf460c9798
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-19 21:09:09 +0200

    feat: add week 20 assignment 7 solution

--- week-20/assignments/159-168.js
+++ week-20/assignments/159-168.js
@@ -94,3 +94,31 @@ console.log(generator.next()); // {value: 2714, done: false}
 console.log(generator.next()); // {value: 3854, done: false}
 console.log(generator.next()); // {value: 5194, done: false}
 console.log(generator.next()); // {value: 6734, done: false}
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 7
+function* genNumbers() {
+  yield* [1, 2, 2, 2, 3, 4, 5];
+}
+function* genLetters() {
+  yield* ['A', 'B', 'B', 'B', 'C', 'D'];
+}
+
+// Write Your Generator Function Here
+function* genAll() {
+  yield* new Set(genNumbers());
+  yield* new Set(genLetters());
+}
+
+generator = genAll();
+
+console.log(generator.next()); // {value: 1, done: false}
+console.log(generator.next()); // {value: 2, done: false}
+console.log(generator.next()); // {value: 3, done: false}
+console.log(generator.next()); // {value: 4, done: false}
+console.log(generator.next()); // {value: 5, done: false}
+console.log(generator.next()); // {value: "A", done: false}
+console.log(generator.next()); // {value: "B", done: false}
+console.log(generator.next()); // {value: "C", done: false}
+console.log(generator.next()); // {value: "D", done: false}
