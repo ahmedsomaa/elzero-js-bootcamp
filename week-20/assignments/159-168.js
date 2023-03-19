f11aeb2c6aadadceeb03f3f8b408d1b0329d6ac8
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-19 19:21:30 +0200

    feat: add week 20 assignment 1 solution

---
+++ week-20/assignments/159-168.js
@@ -0,0 +1,17 @@
+// Assignment 1
+let today = new Date().getTime();
+let myBirthday = new Date("96 3 11").getTime()
+let ageTimestamp = today - myBirthday; 
+let seconds = ageTimestamp / 1000;
+let minutes = seconds / 60;
+let hours = minutes / 60;
+let days = hours / 24;
+let months = days / 12;
+let years = days / 365;
+
+console.log(`${Math.round(seconds)} Seconds`);
+console.log(`${Math.round(minutes)} Minutes`);
+console.log(`${Math.round(hours)} Hours`);
+console.log(`${Math.round(days)} Days`);
+console.log(`${Math.round(months)} Months`);
+console.log(`${Math.round(years)} Years`);
