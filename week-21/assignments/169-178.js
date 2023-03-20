d3615878be54114887ffb6a9dda51aac39bbe03a
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-20 17:53:15 +0200

    feat: add week 21 assignment 3 solution

--- week-21/assignments/169-178.js
+++ week-21/assignments/169-178.js
@@ -1,12 +1,33 @@
 // Assignment 2
+
 let request = new XMLHttpRequest();
 request.open('GET', 'articles.json', true);
 request.send();
 request.onreadystatechange = function () {
   if (this.readyState === this.DONE && this.status == 200) {
     let response = this.responseText;
-    let data = JSON.stringify(response);
+    let data = JSON.parse(response);
     console.log(data);
     console.log('Data Loaded');
+  }
+};
+
+//----------------------------------------------------------------------------------------------------------------------
+
+// Assignment 3
+let xhr = new XMLHttpRequest();
+xhr.open('GET', 'articles.json', true);
+xhr.send();
+xhr.onreadystatechange = function () {
+  if (this.readyState === this.DONE && this.status === 200) {
+    let response = this.responseText;
+    let mainData = JSON.parse(response);
+    for (let i = 0; i < mainData.length; i++) {
+      mainData[i].category = 'All';
+    }
+    console.log(mainData);
+
+    let updatedData = JSON.stringify(mainData);
+    console.log(updatedData);
   }
 };
