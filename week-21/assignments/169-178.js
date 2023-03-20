6ee24ff39b26c9ee3703997e7e9d5bbb2e1576e7
ahmedsomaa <ahmedsomaa@aucegypt.edu>
2023-03-20 18:06:26 +0200

    feat: add week 21 assignment 4 solution

--- week-21/assignments/169-178.js
+++ week-21/assignments/169-178.js
@@ -14,7 +14,7 @@ request.onreadystatechange = function () {
 
 //----------------------------------------------------------------------------------------------------------------------
 
-// Assignment 3
+// Assignment 3 & 4
 let xhr = new XMLHttpRequest();
 xhr.open('GET', 'articles.json', true);
 xhr.send();
@@ -29,5 +29,31 @@ xhr.onreadystatechange = function () {
 
     let updatedData = JSON.stringify(mainData);
     console.log(updatedData);
+
+    // Assignment 4 part
+    const dataDiv = document.createElement('div');
+    dataDiv.id = 'data';
+    for (let i = 0; i < mainData.length; i++) {
+      dataDiv.appendChild(createArticleDiv(mainData[i]));
+    }
+    document.body.appendChild(dataDiv);
   }
 };
+
+// function to create a div element for each article
+function createArticleDiv({ title, author, category, body }) {
+  const articleDiv = document.createElement('div');
+  const articleTitle = document.createElement('h2');
+  articleTitle.textContent = title;
+  const articleBody = document.createElement('p');
+  articleBody.textContent = body;
+  const articleAuthor = document.createElement('p');
+  articleAuthor.textContent = `Author: ${author}`;
+  const articleCategory = document.createElement('p');
+  articleCategory.textContent = `Category: ${category}`;
+  articleDiv.appendChild(articleTitle);
+  articleDiv.appendChild(articleBody);
+  articleDiv.appendChild(articleAuthor);
+  articleDiv.appendChild(articleCategory);
+  return articleDiv;
+}
